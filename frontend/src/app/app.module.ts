45aee10b0b164e579fcafbd3c5da65d4fa015a27
Caio Nóbile <caio.nobile51@gmail.com>
2021-04-18 16:30:52 -0300

    Adicionando "Workspace"

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TreinosComponent } from './treinos/treinos.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TreinosComponent
+    TreinosComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule,
