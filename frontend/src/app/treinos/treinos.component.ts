0ff545a34762105349fb9fc62b69aa2df0a0a727
Victor Lima Xavier <victor.lima.xavier@hotmail.com>
2021-04-26 00:12:49 -0300

    Mudanças no scroll do flip-card

--- frontend/src/app/treinos/treinos.component.ts
+++ frontend/src/app/treinos/treinos.component.ts
@@ -30,7 +30,7 @@ export class TreinosComponent implements OnInit {
     series: 3
   };
 
-  nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
   constructor() { }
 
