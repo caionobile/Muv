378cae242a87812ef1d8ab9524a6a4785a91c410
Caio Nóbile <caio.nobile51@gmail.com>
2021-04-27 00:29:14 -0300

    CalculadoraIMC funcional (sem estilo no resultado)

--- frontend/src/app/calculadora/calculadora.component.ts
+++ frontend/src/app/calculadora/calculadora.component.ts
@@ -12,15 +12,37 @@ export class CalculadoraComponent implements OnInit {
   calcularImc:boolean = false;
   calcularIac:boolean = false;
 
+  resultadoImc:number = 0;
+  resultadoIac:number = 0;
+
+  maskAltura(h):number{
+    h = parseFloat(h)
+    if (h.toString().length == 2)
+      return h/10
+    if (h.toString().length == 3)
+      return h/100
+    return h
+  }
+
+  maskMassa(m):number{
+    m = parseFloat(m)
+    if (m.toString().length >= 3)
+      return m/10
+    return m
+  }
+
   constructor() { }
 
   ngOnInit(): void {
     this.formImc = new FormGroup({
       alturaImc: new FormControl(null, {
-        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(9), Validators.pattern("^[0-9]*$")],
+        validators: [Validators.required],
       }),
-
+      massaImc: new FormControl(null, {
+        validators: [Validators.required],
+      }),
     });
+
     this.formIac = new FormGroup({
       alturaIac: new FormControl(null, {
 
@@ -29,15 +51,21 @@ export class CalculadoraComponent implements OnInit {
     });
   }
 
+  teste():void{
+    console.log(this.maskAltura(this.formImc.value.alturaImc))
+    console.log(this.maskMassa(this.formImc.value.massaImc))
+  }
 
-
-/*   numberOnly(event): boolean {
-    const charCode = (event.which) ? event.which : event.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-      return false;
+  mostrarCalculoImc():void{
+    if(this.formImc.invalid){
+      return
     }
-    return true;
-  } */
+    const altura = this.maskAltura(this.formImc.value.alturaImc);
+    const massa = this.maskMassa(this.formImc.value.massaImc);
+    console.log(altura, massa)
+    this.resultadoImc = this.calculoImc(massa, altura);
+    this.calcularImc = true;
+  }
 
   calculoImc(massa, altura): number {
     const imc = massa/(altura*altura);
@@ -47,7 +75,7 @@ export class CalculadoraComponent implements OnInit {
   }
 
   resetarImc():void{
-    this.calcularImc = !this.calcularImc
+    this.calcularImc = !this.calcularImc;
   }
 
   calculoIac(quadril, altura): number {
