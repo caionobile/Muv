93e1debcdd26475f8851f5fdd827a2aa7ab8e872
Caio Nóbile <caio.nobile51@gmail.com>
2021-05-06 13:09:46 -0300

    Iniciando modais da navbar/sidebar (pelo anchor, por ora)

--- frontend/src/app/bars/bars.component.ts
+++ frontend/src/app/bars/bars.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgForm } from '@angular/forms';
 import * as AOS from 'aos';
 
 @Component({
@@ -7,7 +9,13 @@ import * as AOS from 'aos';
   styleUrls: ['./bars.component.scss']
 })
 export class BarsComponent{
+
+  constructor(
+    private modalService: NgbModal,
+  ){}
+
    opened: boolean = false;
+   closeResult: string;
 
    toggleSidebar() {
     this.opened = !this.opened;
@@ -16,4 +24,24 @@ export class BarsComponent{
   ngOnInit() {
     AOS.init();
   }
+
+    //Abre o modal
+    open(content) {
+      this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+    }
+    //Fecha o modal
+    private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return `with: ${reason}`;
+      }
+    }
+
 }
