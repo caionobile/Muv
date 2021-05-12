28ff11b5b59a23717e709b617f8221029b292028
Vini-iniV <vini.os.contato@gmail.com>
2021-05-12 17:37:48 -0300

    Responsividade da navbar da pagina inicial

--- frontend/src/app/pagina-inicial/navbar-pagina-inicial/navbar-pagina-inicial.component.ts
+++ frontend/src/app/pagina-inicial/navbar-pagina-inicial/navbar-pagina-inicial.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { UsuarioService } from '../auth/usuario.service';
 
 @Component({
   selector: 'app-navbar-pagina-inicial',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarPaginaInicialComponent implements OnInit {
 
-  constructor() { }
+  closeResult: string;
+  form: FormGroup;
+
+  constructor(
+    private modalService: NgbModal,
+    private usuarioService:UsuarioService,
+  ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      nome: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      email: new FormControl(null, {
+        validators: [Validators.required, Validators.email]
+      }),
+      senha: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    })
   }
 
+  onLogin(form: NgForm){
+    if(form.invalid)return;
+    this.usuarioService.login(form.value.email,form.value.password);
+  }
+  onSignup(form: NgForm){
+    console.log(form.value)
+    if(form.invalid)return;
+    this.getDismissReason
+    this.usuarioService.criarUsuario(form.value.nome, form.value.email,form.value.password);
+  }
+
+  //Abre o modal
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  //Fecha o modal
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
