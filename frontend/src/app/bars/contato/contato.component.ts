267fd6a93f1ef3872e5c8130b13d501e8e29b991
Caio Nóbile <caio.nobile51@gmail.com>
2021-05-15 13:02:00 -0300

    Validando inputs

--- frontend/src/app/bars/contato/contato.component.ts
+++ frontend/src/app/bars/contato/contato.component.ts
@@ -1,20 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NgForm, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
-  styleUrls: ['./contato.component.scss']
+  styleUrls: ['./contato.component.scss'],
 })
 export class ContatoComponent implements OnInit {
   formContato!: FormGroup;
   textoEnviado: boolean = false;
-  constructor() { }
+  campoFaltando: boolean = false;
+
+  constructor(public dialogRef: MatDialog) {}
 
   ngOnInit(): void {
+    this.formContato = new FormGroup({
+      assunto: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      mensagem: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+    });
   }
 
-  enviar():void {
+  enviar(): void {
+    if (this.formContato.invalid) {
+      this.textoEnviado = false;
+      this.campoFaltando = true;
+      return;
+    }
+    //=====================ENVIAR EMAIL COM ASSUNTO/MENSAGEM=====================
+
+    //=====================ENVIAR EMAIL COM ASSUNTO/MENSAGEM=====================
+    this.campoFaltando = false;
+    this.textoEnviado = true;
+    setTimeout(() => {
+      this.textoEnviado = false;
+      this.dialogRef.closeAll();
+    }, 1500);
+
 
   }
 }
