0c0d8ae91293c96585fc0be86a094f55811779cd
Caio Nóbile <caio.nobile51@gmail.com>
2021-05-15 11:12:49 -0300

    Adicionando validações de input

--- frontend/src/app/bars/perfil/perfil.component.ts
+++ frontend/src/app/bars/perfil/perfil.component.ts
@@ -8,14 +8,46 @@ import { NgForm, FormGroup, Validators, FormControl } from '@angular/forms';
   styleUrls: ['./perfil.component.scss'],
 })
 export class PerfilComponent implements OnInit {
+  feedback: string = "";
+  validacoes = {
+    senhaRedefinida: "Senha redefinida com sucesso!",
+    novaSenhaIgualAntiga: "A nova senha não pode ser igual a senha anterior",
+    senhaAtualIncorreta: "Senha atual incorreta",
+    campoFaltando: "Preencha todos os campos",
+  }
   formRedefinicao!: FormGroup;
-  senhaRedefinida: boolean = false;
-  senhasIguais: boolean = false;
-  senhaAtualIncorreta: boolean = false;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formRedefinicao = new FormGroup({
+      senhaAtual: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      novaSenha: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+    });
+  }
 
-  redefinirSenha(): void {}
+  redefinirSenha(): void {
+    if (this.formRedefinicao.invalid){
+      this.feedback = this.validacoes.campoFaltando;
+      return
+    }
+    else if (this.formRedefinicao.value.senhaAtual == this.formRedefinicao.value.novaSenha){
+      this.feedback = this.validacoes.novaSenhaIgualAntiga;
+    }
+/*     else if (this.formRedefinicao.value.senhaAtual != "SENHA ANTIGA DO USUARIO"){
+      this.feedback = this.validacoes.senhaAtualIncorreta;
+    } */
+    else {
+      //=====================REDEFINIÇÃO DE SENHA=====================
+
+      //=====================REDEFINIÇÃO DE SENHA=====================
+      this.feedback = this.validacoes.senhaRedefinida;
+      setTimeout(() => {this.feedback = ""}, 5000)
+    }
+  }
+
 }
