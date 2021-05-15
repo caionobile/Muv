29451e41a8f333dd43b790900bd4755b62c276ae
Caio Nóbile <caio.nobile51@gmail.com>
2021-05-15 12:54:17 -0300

    Limpando código

--- frontend/src/app/bars/bars.component.ts
+++ frontend/src/app/bars/bars.component.ts
@@ -41,28 +41,4 @@ export class BarsComponent {
       width: '650px',
     });
   }
-
-  //Abre o modal
-  open(content) {
-    this.modalService
-      .open(content, { ariaLabelledBy: 'modal-basic-title' })
-      .result.then(
-        (result) => {
-          this.closeResult = `Closed with: ${result}`;
-        },
-        (reason) => {
-          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        }
-      );
-  }
-  //Fecha o modal
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
 }
