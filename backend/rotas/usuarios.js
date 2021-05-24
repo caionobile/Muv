eda20088d68d1ba84f423777a6c013855b8d7a6e
Caio Nóbile <caio.nobile51@gmail.com>
2021-05-24 20:16:17 -0300

    Corrigindo endpoint de login

--- backend/rotas/usuarios.js
+++ backend/rotas/usuarios.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Usuario = require("../models/usuario");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //Conexão Usuario
 router.get("/:id", (req, res, next) => {
   Usuario.findById(req.params.id).then((usu) => {
-    console.log(usu)
+    console.log(usu);
     if (usu) {
       res.status(200).json(usu);
     } else {
@@ -25,52 +25,50 @@ router.delete("/:id", (req, res, next) => {
   });
 });
 
-router.post('/login',(req,res,next)=>{
+router.post("/login", (req, res, next) => {
   let user;
-  Usuario.findOne({email:req.body.email}).then(u=>{
-    user=u;
-    console.log (user)
-    if(!u){
+  Usuario.findOne({ email: req.body.email })
+    .then((u) => {
+      user = u;
+      return bcrypt.compare(req.body.senha, u.senha);
+    })
+    .then(() => {
+      const token = jwt.sign({ email: user.email, id: user.id }, "minhasenha", {
+        expiresIn: "1h",
+      });
+      res.status(200).json({
+        nome: user.nome,
+        email: user.email,
+        id: user.id,
+        token: token,
+      });
+    })
+    .catch(() => {
       return res.status(401).json({
-        mensagem:"Email invalido"
-      })
-    }
-    return bcrypt.compare(req.body.senha,u.senha);
-  }).then(result=>{
-    if(!result){
-    return res.status(401).json({
-      mensagem:"Senha invalida"
-    })
-  }
-    const token = jwt.sign(
-      {email:user.email,id:user.id},
-      'minhasenha',
-      {expiresIn:'1h'}
-    )
-    res.status(200).json({nome:user.nome,email:user.email,id:user.id,token:token})
-  }).catch(err=>{
-    return res.status(401).json({
-      mensagem:"Login falhou: "+err
-    })
-  })
-})
+        mensagem: "Email e/ou senha incorretos",
+      });
+    });
+});
 
-router.post('/signup',(req, res,next) => {
-  bcrypt.hash(req.body.senha, 10).then(hash =>{
-  const usuario= new Usuario({
-    nome: req.body.nome,
-    email: req.body.email,
-    senha: hash
-  })
-  usuario.save().then(result =>{
-    res.status(201).json({
-      mensagem:"Usuario criado",
-      resultado: result
-    })
-  }).catch(err =>{
-    res.status(500).json({error:err})
-  })
-  })
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.senha, 10).then((hash) => {
+    const usuario = new Usuario({
+      nome: req.body.nome,
+      email: req.body.email,
+      senha: hash,
+    });
+    usuario
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          mensagem: "Usuario criado",
+          resultado: result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err });
+      });
+  });
 });
 
 module.exports = router;
