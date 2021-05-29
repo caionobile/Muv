aa0f4fb30c8450787a1f522b8ed34e7ca566eefe
Victor Lima Xavier <victor.lima.xavier@hotmail.com>
2021-05-29 17:02:22 -0300

    Atualização dos trienos

--- frontend/src/app/treinos/treinos.component.ts
+++ frontend/src/app/treinos/treinos.component.ts
@@ -10,48 +10,27 @@ import { TreinoService } from '../service/treino.service';
 })
 export class TreinosComponent implements OnInit {
   treinos: Treino[] = [];
-  treinosPersonalizados: Treino[] = [];
-  treinosPredefinidos: Treino[] = [];
   novaOrdem: Treino[];
   private treinosSubscription: Subscription;
 
   ngOnInit(): void {
-    const idUsuario = localStorage.getItem('id');
-    this.treinoService.getTreinos(idUsuario);
+  const idUsuario = localStorage.getItem("id")
+  this.treinoService.getTreinos(idUsuario);
     this.treinosSubscription = this.treinoService
-      .getListaDeTreinosAtualizadaObservable()
-      .subscribe((treinos: Treino[]) => {
-        this.treinosPersonalizados = treinos.filter(
-          (treino) => treino.assignTo === localStorage.getItem('id')
-        );
-        this.treinosPredefinidos = treinos.filter(
-          (treino) => treino.assignTo != localStorage.getItem('id')
-        );
-        this.treinos = this.treinosPersonalizados;
-      });
+    .getListaDeTreinosAtualizadaObservable()
+    .subscribe((treinos: Treino[]) => {
+      this.treinos = treinos;
+    });
   }
 
   constructor(public treinoService: TreinoService) {}
 
   storeNewOrder(event) {
-    if (event.currentOrder[0].assignTo === localStorage.getItem('id')) {
-      let ordem = []
-      for (let i = 0; i < event.currentOrder.length; i++) {
-        if(event.currentOrder[i] != undefined) {
-          event.currentOrder[i].posicao = i;
-          ordem.push(event.currentOrder[i])
-        }
-      }
-      this.novaOrdem = ordem;
-      this.treinoService.atualizarPosicaoDeTreinos(this.novaOrdem);
+    for (let i = 0; i < event.currentOrder.length; i++) {
+      event.currentOrder[i].posicao = i;
     }
+    this.novaOrdem = event.currentOrder;
+    this.treinoService.atualizarPosicaoDeTreinos(this.novaOrdem)
   }
 
-  mostrarPersonalizados() {
-    this.treinos = this.treinosPersonalizados;
-  }
-
-  mostrarPredefinidos() {
-    this.treinos = this.treinosPredefinidos;
-  }
 }
